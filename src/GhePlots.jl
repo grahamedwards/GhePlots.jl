9e985a4d5afe5e43baa74bba8d402ca4ab9d1672
Graham H. Edwards <grahamhedwards92@gmail.com>
2025-10-07 12:06:52 -0500

    move figtext funcs out of module, bump darkmode fontsize to 24

--- src/GhePlots.jl
+++ src/GhePlots.jl
@@ -3,7 +3,7 @@ module GhePlots
     import ColorSchemes 
     import Makie as Mke
 
-    export dark_mode, light_mode, FigText
+    export dark_mode, light_mode, sub, sup, siground
 
     """
 
@@ -13,7 +13,7 @@ module GhePlots
     """
     dark_mode() = Mke.Theme(
 
-        fonts=(;regular="IBM Plex Sans Regular",bold="IBM Plex Sans Bold",italic="IBM Plex Sans Italic"),fontsize = 14, 
+        fonts=(;regular="IBM Plex Sans Regular",bold="IBM Plex Sans Bold",italic="IBM Plex Sans Italic"),fontsize = 24, 
 
         backgroundcolor=:transparent, labelcolor=:white, palette = (; color = ColorSchemes.tol_bright),
         
@@ -58,89 +58,74 @@ module GhePlots
     )
 
 
+    """
+        
+        sub(I)
+
+    Return a unicode string subscript of Integer `I`. 
+
+    """
+    function sub(I::Int)
+        c = I < 0 ? [Char(0x208B)] : []
+
+        for d in reverse(digits(abs(I)))
+            push!(c, Char(0x2080+d))
+        end
+        return join(c)
+    end
+
+
+    """
+        
+        sup(I)
+
+    Return a unicode string superscript of Integer `I`. 
+
+    """
+    function sup(I::Int)
+        c = I < 0 ? [Char(0x207B)] : []
+
+        for d in reverse(digits(abs(I)))
+            d == 0 && push!(c, Char(0x2070))
+            d == 1 && push!(c, Char(0x00B9))
+            d == 2 && push!(c, Char(0x00B2))
+            d == 3 && push!(c, Char(0x00B3))
+            d >  3 && push!(c, Char(0x2070+d))
+        end
+        return join(c)
+    end
+
 
     """
 
-    The FigText module contains functions that make adding text to tables and figures prettier, easier: 
-    - `FigText.sub` (subscript)
-    - `FigText.sup` (superscript)
-    - `FigText.siground`
+        siground(x,u,sigfigs=3)
 
-    Use `help?` for more info.
+    Round a measurement `x` and its uncertainty `u` to `sigfigs`. Accepts a single value or a `Tuple` of values for `u`.
+
+    Returns a `Tuple` of `(x,u...)`
+
+    e.g.
+        julia> siground(523.12, (15.8,278.0) )
+        (523, 16, 278)
 
     """
-    module FigText
+    function siground(x::Number, u; sigfigs::Int=3)
+        @assert length(u) >= 1
 
-        """
-            
-            FigText.sub(I)
+        x = round(x,sigdigits=sigfigs)
 
-        Return a unicode string subscript of Integer `I`. 
-
-        """
-        function sub(I::Int)
-            c = I < 0 ? [Char(0x208B)] : []
-
-            for d in reverse(digits(abs(I)))
-                push!(c, Char(0x2080+d))
-            end
-            return join(c)
+        if isinteger(x)
+            x = Int(x)
+            u = round.(Int,u)
+            Δdigits = ndigits(x) .- ndigits.(u)
+            u = (( Int(round(u[i],sigdigits = sigfigs - Δdigits[i])) for i in eachindex(u))..., )
+        else
+            ur = round.(u, sigdigits=sigfigs)
+            _,xex = Base.Ryu.reduce_shortest(x)
+            uex = ((Base.Ryu.reduce_shortest(i)[2] for i in ur)...,)
+            u = ((round(u[i],sigdigits = sigfigs - (xex-uex[i])) for i in eachindex(u))...,)
         end
-
-
-        """
-            
-            FigText.sup(I)
-
-        Return a unicode string superscript of Integer `I`. 
-
-        """
-        function sup(I::Int)
-            c = I < 0 ? [Char(0x207B)] : []
-
-            for d in reverse(digits(abs(I)))
-                d == 0 && push!(c, Char(0x2070))
-                d == 1 && push!(c, Char(0x00B9))
-                d == 2 && push!(c, Char(0x00B2))
-                d == 3 && push!(c, Char(0x00B3))
-                d >  3 && push!(c, Char(0x2070+d))
-            end
-            return join(c)
-        end
-
-
-        """
-
-            FigText.siground(x,u,sigfigs=3)
-
-        Round a measurement `x` and its uncertainty `u` to `sigfigs`. Accepts a single value or a `Tuple` of values for `u`.
-
-        Returns a `Tuple` of `(x,u...)`
-
-        e.g.
-            julia> siground(523.12, (15.8,278.0) )
-            (523, 16, 278)
-
-        """
-        function siground(x::Number, u; sigfigs::Int=3)
-            @assert length(u) >= 1
-
-            x = round(x,sigdigits=sigfigs)
-
-            if isinteger(x)
-                x = Int(x)
-                u = round.(Int,u)
-                Δdigits = ndigits(x) .- ndigits.(u)
-                u = (( Int(round(u[i],sigdigits = sigfigs - Δdigits[i])) for i in eachindex(u))..., )
-            else
-                ur = round.(u, sigdigits=sigfigs)
-                _,xex = Base.Ryu.reduce_shortest(x)
-                uex = ((Base.Ryu.reduce_shortest(i)[2] for i in ur)...,)
-                u = ((round(u[i],sigdigits = sigfigs - (xex-uex[i])) for i in eachindex(u))...,)
-            end
-            (x,u...)
-        end
-
-    end 
+        (x,u...)
+    end
 
 end
